2c2b100465f0f6abbfe653b8be31939e5211250f
munair <munair@qe.local>
2016-06-07 11:16:01 +0900

    test page header in 404.php header

--- 404.php
+++ 404.php
@@ -1,9 +1,7 @@
-<?php get_template_part('templates/header', 'header'); ?>
+<?php get_template_part('templates/page', 'header'); ?>
 
 <section id="404" class="404">
 	<div class="alert alert-warning">
-	  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+	  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?> <?php get_search_form(); ?>
 	</div>
 </section>
-
-<?php get_search_form(); ?>
