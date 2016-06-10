497348cf7b18966dadca3ec91f570d2be95d9bda
munair <munair@qe.local>
2016-06-10 17:33:40 +0900

    fader was the bug

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
                <header class="header">
                         <!-- slider animation -->
-                        <ul class="keyframe-post-fader">
+                        <ul class="keyframe-fader">
                                 <li></li>
                                 <li></li>
                                 <li></li>
