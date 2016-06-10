4a608dbe9a0075c7ec71163a767ccea0de02acec
munair <munair@qe.local>
2016-06-10 17:25:08 +0900

    add new header

--- templates/header.php
+++ templates/header.php
@@ -1,22 +1,22 @@
-<header class="header">
-	<div class="header-content">
-		<div class="header-content-inner">
-			<h1>Creative Movement.<br> Serious Fitness.</h1>
-			<hr>
-			<p>dance, play, sing and above all move creatively</p>
-			<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Find Out More</a>
-			<hr>
-			<h3 class="white-icons">
-				<div class="col-xs-4">
-					<a href="https://capoeirastudio.typeform.com/to/veVkqb"><i class="fa fa-child"></i></a> KIDS CLASS
-				</div>
-				<div class="col-xs-4">
-					<a href="mailto:learnprivately@capoeirastudio.com"><i class="fa fa-male"></i></a> LEARN PRIVATELY
-				</div>
-				<div class="col-xs-4">
-					<a href="mailto:freetrial@capoeirastudio.com"><i class="fa fa-check-circle-o"></i></a> FREE TRIAL
-				</div>
-			</h3>
-		</div>
-	</div>
-</header>
+               <header class="header">
+                        <!-- slider animation -->
+                        <ul class="keyframe-post-fader">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                        </ul>
+                        <div class="v-center">
+                                <a onmousedown="tick()" onmouseover="pop()" href="<?= esc_url(home_url('/')); ?>">
+                                        <img class="scroll-down" src="https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/capoeirastudio-whitealpha-icon-600x600px.png" />
+                                </a>
+                                <h1><?php bloginfo('name'); ?></h1>
+                                <h3 class="wow zoomIn" data-wow-delay="2s"><?php bloginfo('name'); ?></h3>
+                                <nav class="nav-primary">
+                                        <?php
+                                                if (has_nav_menu('primary_navigation')) :
+                                                        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+                                                endif;
+                                        ?>
+                                </nav>
+                        </div>
+                </header>
