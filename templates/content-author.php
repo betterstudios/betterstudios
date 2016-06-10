cc2ad861cb1adac382a4b312350f0188896118bb
munair <munair@qe.local>
2016-06-10 19:43:04 +0900

    remove unnecessary templates

--- templates/content-author.php
+++
@@ -1,5 +0,0 @@
-<div class="author_bio">
-	<h4><?php _e( 'Author Biography:', 'capoeirastudio'); ?></h4>
-	<div class="title-divider"></div>
-	<p><?php the_author_meta( 'description' , $capoeirastudio_uid ); ?></p>
-</div>
