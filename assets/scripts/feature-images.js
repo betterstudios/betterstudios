5a18085a22b65fd4cc468d396dfe2add3626dac8
munair <munair@qe.local>
2016-06-11 15:45:32 +0900

    testing new jquery

--- assets/scripts/feature-images.js
+++ assets/scripts/feature-images.js
@@ -9,8 +9,14 @@
 
 $('.button').click(function(){
   // Handler for .load() called.
-  var images = $('#featured-image').attr('src');
+  var images = $('featured-image').attr('src');
   $('head').append('<meta property="og:image" content="' + images + '" />');      
+});
+
+$( "img" ).load(function() {
+  if ( $( this ).height() > 100) {
+    $( this ).addClass( "bigImg" );
+  }
 });
 
 })(jQuery); // End of use strict
