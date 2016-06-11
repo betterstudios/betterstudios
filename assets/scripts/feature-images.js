9320b52e468528116ed18792c247289143d295a1
munair <munair@qe.local>
2016-06-11 16:05:51 +0900

    test javascript that works on the post

--- assets/scripts/feature-images.js
+++ assets/scripts/feature-images.js
@@ -2,25 +2,6 @@
  * Feature Images
  */
 
-(function($) {
-    "use strict"; // Start of use strict
+var imgsrc = document.getElementById("myImg").src;
+alert(imgsrc);
 
-    // jQuery for the slideshow in the header section.
-
-$('.button').click(function(){
-  // Handler for .load() called.
-  var images = $('featured-image').attr('src');
-  $('head').append('<meta property="og:image" content="' + images + '" />');      
-});
-
-$("#myImg").load(function() {
-  alert('I loaded!');
-}).attr('src', 'myImage.jpg');
-
-$( "img" ).load(function() {
-  if ( $( this ).height() > 100) {
-    $( this ).addClass( "bigImg" );
-  }
-});
-
-})(jQuery); // End of use strict
