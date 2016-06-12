68e6ab04aac0514643907211b58c22a526552607
munair <munair@qe.local>
2016-06-12 10:05:43 +0900

    jquery not working... going to work on another solution.

--- assets/scripts/feature-images.js
+++ assets/scripts/feature-images.js
@@ -2,6 +2,15 @@
  * Feature Images
  */
 
-var imgsrc = document.getElementById("myImg").src;
-alert(imgsrc);
+(function($) {
+    "use strict"; // Start of use strict
 
+    // jQuery for the slideshow in the header section.
+
+$( "img" ).load(function() {
+  if ( $( this ).height() > 100) {
+    $( this ).addClass( "bigImg" );
+  }
+});
+
+})(jQuery); // End of use strict
