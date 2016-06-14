eebcef38b70de60cb48db8a41b3f9adb9b673aaa
munair <munair@qe.local>
2016-06-15 03:10:47 +0900

    remove wrapping

--- assets/scripts/feature-images.js
+++ assets/scripts/feature-images.js
@@ -2,12 +2,6 @@
  * Feature Images
  */
 
-(function($) {
-	"use strict"; // Start of use strict
-
-	// jQuery for the slideshow in the header section.
 	var imgSrc = $('#featured-image').attr('src');
 	$('.keyframe-post-fader').html("<li></li>");
 	$('.keyframe-post-fader').css('background-image', 'url(' + imgSrc  + ')');
-
-})(jQuery); // End of use strict
