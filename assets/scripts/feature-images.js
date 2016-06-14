5c0f7d5699ca66ae4900eee7a83510efaa7067c2
munair <munair@qe.local>
2016-06-14 18:30:06 +0900

    again

--- assets/scripts/feature-images.js
+++ assets/scripts/feature-images.js
@@ -3,8 +3,7 @@
  */
 
 (function(){
-	var imageUrl = document.querySelector('img').src;
-	document.getElementById('header-background').innerHTML = '<ul><li></li></ul>';
+	var imageUrl = document.querySelector('#featured-image').src;
 	document.querySelector('#header-background').innerHTML = '<ul><li></li></ul>';
 	document.querySelector('#header-background.ul.li').style.backgroundImage = 'url(' + imageUrl + ')';
 })();
