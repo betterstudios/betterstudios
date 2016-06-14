43b2f444852d8d9775bf8ea38f1b12b1f7fb4e92
munair <munair@qe.local>
2016-06-15 02:31:48 +0900

    last try

--- assets/scripts/feature-images.js
+++ assets/scripts/feature-images.js
@@ -7,7 +7,7 @@
 
 	// jQuery for the slideshow in the header section.
 	var imgSrc = $('#featured-image').attr('src');
-	$('.keyframe-post-fader').html('<ul><li></li></ul>');
+	$('.keyframe-post-fader').html('<li></li>');
 	$('.keyframe-post-fader').css('background-image', 'url(' + imgSrc  + ')');
 
 })(jQuery); // End of use strict
