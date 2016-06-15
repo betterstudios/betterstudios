a0be3ff18bc89197250a7ed8be58fc517be4ba0f
munair <munair@qe.local>
2016-06-16 02:19:50 +0900

    it's alive... clean up

--- assets/scripts/feature-images.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Feature Images
- */
-
-	var imgSrc = $('#featured-image').attr('src');
-	$('.keyframe-post-fader').html("<li></li>");
-	$('.keyframe-post-fader').css('background-image', 'url(' + imgSrc  + ')');
