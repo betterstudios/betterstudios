aa6624b053f492b49fdd4886cf749870dd4c7308
munair <munair@qe.local>
2016-06-16 00:47:59 +0900

    use more appropriate javascript

--- templates/postscript.php
+++ templates/postscript.php
@@ -1,10 +1,16 @@
 <!-- Scripts for Beautifying Posts -->
 <script type="text/javascript">
 (function(){
+	// get the source url of the imaged with the id "featured-image"
 	var imgUrl = document.getElementById("featured-image").src;
-	var bkgDiv = document.querySelector(".keyframe-post-fader");
-	bkgDiv.innerHTML = "<li></li>";
-	bkgDiv.style.backgroundImage = "url('" + imgUrl + "')";
+
+	// remove the child nodes 0 and 1
+	var list = document.getElementById("keyframe-post-fader"); 
+	list.removeChild(list.childNodes[0]);
+	list.removeChild(list.childNodes[1]);
+	
+	// assign new background image to the first list item
+	list.querySelector("li").style.backgroundImage = "url('" + imgUrl + "')";
 })();
 </script>
 
