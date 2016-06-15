ed52a54672db64b35e1d701cc1c202905a5f1623
munair <munair@qe.local>
2016-06-16 00:11:26 +0900

    test using document.getElementById("myImg").src

--- templates/postscript.php
+++ templates/postscript.php
@@ -1,7 +1,7 @@
 <!-- Scripts for Beautifying Posts -->
 <script type="text/javascript">
 (function(){
-	var imgUrl = document.querySelector("#featured-image").src;
+	var imgUrl = document.getElementById("featured-image").src;
 	var bkgDiv = document.querySelector(".keyframe-post-fader");
 	bkgDiv.innerHTML = "<li></li>";
 	bkgDiv.style.backgroundImage = "url('" + imgUrl + "')";
