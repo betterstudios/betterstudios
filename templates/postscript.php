132232be4a573020c40c5fdade47016352e078c3
munair <munair@qe.local>
2016-06-16 00:01:59 +0900

    add function wrapper

--- templates/postscript.php
+++ templates/postscript.php
@@ -1,10 +1,10 @@
 <!-- Scripts for Beautifying Posts -->
 <script type="text/javascript">
-	/* <![CDATA[ */
+(function(){
 	var imgUrl = document.querySelector("#featured-image").src;
 	var bkgDiv = document.querySelector(".keyframe-post-fader");
 	bkgDiv.innerHTML = "<li></li>";
 	bkgDiv.style.backgroundImage = "url('" + imgUrl + "')";
-	/* ]]> */
+})();
 </script>
 
