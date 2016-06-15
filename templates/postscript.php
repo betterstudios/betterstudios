1cab567ce52579f9e2e76b49fcdcb37b9efa98c0
munair <munair@qe.local>
2016-06-16 01:47:56 +0900

    testing new javascript

--- templates/postscript.php
+++ templates/postscript.php
@@ -4,12 +4,23 @@
 	// get the source url of the imaged with the id "featured-image"
 	var imgUrl = document.getElementById("featured-image").src;
 
-	// remove the child nodes 0 and 1
+	// remove the child nodes 0, 1 and 2
 	var list = document.getElementById("backgrounds"); 
-	list.removeChild(list.childNodes);
-	
+	list.removeChild(list.childNodes[0]);
+	list.removeChild(list.childNodes[1]);
+	list.removeChild(list.childNodes[2]);
+
+	// create new list item	
+	var listItem = document.createElement("li");
+
+	// give the new list item an id attribute called 'background'
+	listItem.id = "background";
+
+	// now append the new list item to the unordered list
+	list.appendChild(listItem);
+
 	// assign new background image to the first list item
-	list.style.backgroundImage = "url('" + imgUrl + "')";
+	listItem.style.backgroundImage = "url('" + imgUrl + "')";
 })();
 </script>
 
