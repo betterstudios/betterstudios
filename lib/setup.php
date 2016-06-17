e79251759b548ab398e064357fb38f85a48cc768
munair <munair@qe.local>
2016-06-17 21:04:11 +0900

    add FAQ page

--- lib/setup.php
+++ lib/setup.php
@@ -84,6 +84,7 @@ function display_sidebar() {
     // The sidebar will NOT be displayed if ANY of the following return true.
     // @link https://codex.wordpress.org/Conditional_Tags
     is_404(),
+    is_faq(),
     is_search(),
     is_author(),
     is_front_page(),
