8af3febce49b1c2684069ef12d4853d7b846c732
munair <munair@qe.local>
2016-06-18 08:48:29 +0900

    need to add faq base/template

--- lib/setup.php
+++ lib/setup.php
@@ -86,7 +86,7 @@ function display_sidebar() {
     is_404(),
     is_search(),
     is_author(),
-    is_faq_page(),
+    is_faq(),
     is_front_page(),
     is_page_template('template-custom.php'),
   ]);
