b2fdff592c6157849841371cd8345e09398ef22a
munair <munair@qe.local>
2016-06-19 00:18:02 +0900

    correct spelling mistake

--- templates/blog-contributors.php
+++ templates/blog-contributors.php
@@ -5,7 +5,7 @@
 				<p>지금 보시는 블로그는 카포에라 스튜디오의 학생들과 선생님들이 업데이트·관리를 하고 있습니다. 현재 저희 페이스북 그룹에 활발하게 자료를 올리시거나, 카포에라 지식을 조사하고 공유하는 것을 좋아하는 분들은 블로그의 필진이 될 수 있습니다. 신청하세요.</p>
 				<p>This blog is maintained by the students and teachers at the Capoeira Studio. Are you actively posting content in our Facebook Group? Do you enjoy researching and sharing Capoeira knowledge? Then apply to be a contributor.</p>
 				<hr class="primary">
-                                <a class="typeform-share link btn btn-default btn-xl bg-orange wow bounceIn" data-wow-delay=".5s" href="https://capoeirastudio.typeform.com/to/Jfyh3n" data-mode="1" target="_blank">기여 <br>Contribute</a>
+                                <a class="typeform-share link btn btn-default btn-xl bg-orange wow bounceIn" data-wow-delay=".5s" href="https://capoeirastudio.typeform.com/to/Jfyh3n" data-mode="1" target="_blank">참여하기 <br>Contribute</a>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
 			</div>
 		</div>
