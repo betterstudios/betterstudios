c27ae5a8858693e3be6ecffee4da8889fba15dcd
munair <munair@qe.local>
2016-06-19 02:17:14 +0900

    update blog contributors grid to match that of the_content

--- templates/blog-contributors.php
+++ templates/blog-contributors.php
@@ -1,7 +1,7 @@
 <section id="blog-contributors" class="blog-contributors-section front-page">
 	<div class="container">
 		<div id="contributor-row" class="row">
-			<div class="col-sm-8 col-lg-offset-2 text-center">
+			<div class="col-sm-8 col-sm-offset-2 text-center">
 				<p>지금 보시는 블로그는 카포에라 스튜디오의 학생들과 선생님들이 업데이트·관리를 하고 있습니다. 현재 저희 페이스북 그룹에 활발하게 자료를 올리시거나, 카포에라 지식을 조사하고 공유하는 것을 좋아하는 분들은 블로그의 필진이 될 수 있습니다. 신청하세요.</p>
 				<p>This blog is maintained by the students and teachers at the Capoeira Studio. Are you actively posting content in our Facebook Group? Do you enjoy researching and sharing Capoeira knowledge? Then apply to be a contributor.</p>
 				<hr class="primary">
