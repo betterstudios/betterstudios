a409b69a3a46846b98950f78e8237e5556fe74c5
munair <munair@qe.local>
2016-06-18 20:27:34 +0900

    simplify blog and resize blog page header fonts

--- templates/blog-widgets.php
+++ templates/blog-widgets.php
@@ -1,17 +1,11 @@
 <section id="blog-widgets" class="blog-widgets-section">
 	<div class="container-fluid">
 		<div id="widget-row" class="row">
-	                <div class="col-sm-2 col-sm-offset-1">
+	                <div class="col-sm-2 col-sm-offset-3">
 	                        <?php the_widget( 'WP_Widget_Categories', $instance, $args ); ?>
 	                </div>
 	                <div class="col-sm-2">
-	                        <?php the_widget( 'WP_Widget_Tag_Cloud', $instance, $args ); ?>
-	                </div>
-	                <div class="col-sm-2">
 	                        <?php the_widget( 'WP_Widget_Recent_Posts', $instance, $args ); ?>
-	                </div>
-	                <div class="col-sm-2">
-	                        <?php the_widget( 'WP_Widget_Links', $instance, $args ); ?>
 	                </div>
 	                <div class="col-sm-2">
 	                        <?php the_widget( 'WP_Widget_Archives', $instance, $args ); ?>
