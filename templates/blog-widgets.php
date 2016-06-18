2817b1d2ce2f6f1d22ba4b65607731d7151c9a6a
munair <munair@qe.local>
2016-06-18 10:59:28 +0900

    use the introduction template as a starter for the blog-widget template

--- templates/blog-widgets.php
+++ templates/blog-widgets.php
@@ -1,30 +1,14 @@
-<section id="blog-widgets" class="blog">
-	<div class="container-fluid">
-	        <div class="row">
-	                <div class="col-sm-4">
-	                        <?php the_widget( 'WP_Widget_Links', $instance, $args ); ?>
-	                </div>
-	                <div class="col-sm-4">
-	                        <?php get_calendar(); ?>
-	                </div>
-	                <div class="col-sm-4">
-	                        <?php $args = array(
-	                            'numberposts' => 10,
-	                            'offset' => 0,
-	                            'category' => 0,
-	                            'orderby' => 'post_date',
-	                            'order' => 'DESC',
-	                            'include' => ,
-	                            'exclude' => ,
-	                            'meta_key' => ,
-	                            'meta_value' =>,
-	                            'post_type' => 'post',
-	                            'post_status' => 'publish',
-	                            'suppress_filters' => true );
-	
-	                            $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
-	                        ?>
-	                </div>
-	        </div>
+<section class="bg-primary" id="intro">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 text-center">
+				<h2 class="section-heading">카포에라 스튜디오</h2>
+				<hr class="light">
+				<p class="text-faded">Capoeira Studio의 강사진들과 수강생들은 카포에라라고 불리는 아프로 브라질리언 예술 행위의 창조적이고 아름다운 무브먼트를 통해 강인함, 유연성, 민첩성, 균형감각, 집중력, 리듬감 그리고 삶의 질을 향상시키는 것에 집중을 합니다.</p>
+				<p class="text-faded">The community of teachers and students at the Capoeira Studio are focused on improving strength, flexibility, agility, balance, concentration, rhythm, and quality of life through the creative and beautiful movement of the Afro-Brazilian artform called Capoeira.</p>
+				<hr class="light">
+				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Learn More</a>
+			</div>
+		</div>
 	</div>
 </section>
