87216b44510ab2a81eee2b3c89054b7dd79acc5e
munair <munair@qe.local>
2016-06-18 10:48:25 +0900

    clean up template for the blog by following the sage guideline

--- base-page-blog.php
+++ base-page-blog.php
@@ -24,10 +24,15 @@ use Roots\Sage\Wrapper;
 	        		<main class="main">
 	          			<?php include Wrapper\template_path(); ?>
 	        		</main><!-- /.main -->
+	        		<?php if (Setup\display_sidebar()) : ?>
+		          		<aside class="sidebar">
+		            			<?php include Wrapper\sidebar_path(); ?>
+		          		</aside><!-- /.sidebar -->
+	        		<?php endif; ?>
 			</div><!-- /.row -->
       		</div><!-- /.container -->
     	</section><!-- /.primary -->
-	<?php get_template_part('templates/blog-widgets'); ?>
+	<?php get_template_part('templates/introduction'); ?>
     	<?php
 		do_action('get_footer');
 		get_template_part('templates/footer');
