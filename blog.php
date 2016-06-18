bd1b68a5e2fcc5bad0a48d80ca142ced5386fab2
munair <munair@qe.local>
2016-06-18 09:48:07 +0900

    add to base

--- blog.php
+++ blog.php
@@ -9,36 +9,6 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-sm-4">
-			<?php the_widget( 'WP_Widget_Links', $instance, $args ); ?> 
-		</div>
-		<div class="col-sm-4">
-			<?php get_calendar(); ?>
-		</div>
-		<div class="col-sm-4">
-			<?php $args = array(
-			    'numberposts' => 10,
-			    'offset' => 0,
-			    'category' => 0,
-			    'orderby' => 'post_date',
-			    'order' => 'DESC',
-			    'include' => ,
-			    'exclude' => ,
-			    'meta_key' => ,
-			    'meta_value' =>,
-			    'post_type' => 'post',
-			    'post_status' => 'publish',
-			    'suppress_filters' => true );
-			
-			    $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
-			?>
-		</div>
-	</div>
-</div>
-
 <?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
