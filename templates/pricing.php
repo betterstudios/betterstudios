2a19b868ce242750b8b67fa47177ab871352b2a1
munair <munair@qe.local>
2016-06-19 13:20:06 +0900

    upload new typeform

--- templates/pricing.php
+++ templates/pricing.php
@@ -13,8 +13,8 @@
 				<p>연 회원 등록이 부담스러우셔도 문제 없습니다. 1달 수업료는 250,000원입니다. 언제든 등록하셔서 수업에 참석해 보시기 바랍니다.</p>
 				<p>Our Yearly payment plans is "rolling" and needn't coincide with the start of a month or the year. In other words, if you sign up for Yearly membership on September 15, 2015 you may attend all scheduled classes until September 15, 2016. You can even come twice a day!</p>
 				<p>Can't afford the year? No problem. A month of unlimited classes is only 250,000 KRW. Join us anytime your schedule allows.</p>
-				<a class="typeform-share btn btn-primary btn-xl page-scroll wow bounceIn" href="https://zumbi.typeform.com/to/gu3N7u" data-mode="1" target="_blank">신청하세요 <br>Apply Now</a>
-				<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>
+				<a class="typeform-share btn btn-primary btn-xl wow bounceIn" href="https://capoeirastudio.typeform.com/to/Jfyh3n" data-mode="1" target="_blank">신청하세요 <br>Apply Now</a>
+				<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
 			</div>
 		</div>
 	</div>
