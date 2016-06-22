4c18396d41aa11a53ee962db52eb0522237d834d
munair <munair@qe.local>
2016-06-23 08:47:46 +0900

    test new wobbling arrow

--- templates/content-single.php
+++ templates/content-single.php
@@ -36,6 +36,9 @@ while (have_posts()) : the_post(); ?>
 						<span><a href="#blog-entry">share</a></span>
 						<span><a href="#blog-entry">contribute</a></span>
 					</div>
+					<div class="wobbling-arrow">
+						<i class="fa fa-arrow-down" aria-hidden="true"></i>
+					</div>
 			</div>
 		        <div class="v-center">
 				<div class="top">
