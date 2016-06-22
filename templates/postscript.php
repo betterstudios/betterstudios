5230870ea14fc618679f73c24c2fff5b27bd95f7
munair <munair@qe.local>
2016-06-22 13:30:42 +0900

    add author and featured image

--- templates/postscript.php
+++ templates/postscript.php
@@ -1,6 +1,26 @@
 <!-- Scripts for Beautifying Posts -->
 <script type="text/javascript">
 (function(){
+	// get the source url of the imaged with the id "featured-image"
+	var imgUrl = document.getElementById("featured-image").src;
+
+	// remove the child nodes 0, 1 and 2
+	var list = document.getElementById("backgrounds"); 
+	list.removeChild(list.childNodes[0]);
+	list.removeChild(list.childNodes[1]);
+	list.removeChild(list.childNodes[2]);
+
+	// create new list item	
+	var listItem = document.createElement("li");
+
+	// give the new list item an id attribute called 'background'
+	listItem.id = "background";
+
+	// now append the new list item to the unordered list
+	list.appendChild(listItem);
+
+	// assign new background image to the first list item
+	listItem.style.backgroundImage = "url('" + imgUrl + "')";
 })();
 </script>
 
