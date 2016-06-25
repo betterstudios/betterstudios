9d72ada4fe7c45cda87ab2e43f8ead3e69a55564
munair <munair@qe.local>
2016-06-25 18:03:00 +0900

    test delaying each vegas slide

--- assets/scripts/vegas.js
+++ assets/scripts/vegas.js
@@ -9,14 +9,14 @@
 
 $(".front-page-header").vegas({
       timer: false,
-      delay: 700,
+      delay: 100,
       preload: true,
       overlay: 'https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/07.png',
       slides: [
-        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "70%", transition: "zoomOut" },
-        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "50%", transition: "swirlRight" },
-        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "30%", transition: "zoomOut" },
-        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", align: "10%", transition: "blur" },
+        { delay: 700, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "70%", transition: "zoomOut" },
+        { delay: 1700, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "50%", transition: "swirlRight" },
+        { delay: 1700, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "30%", transition: "zoomOut" },
+        { delay: 1700, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", align: "10%", transition: "blur" },
       ],
       animation: 'kenburnsUpLeft',
       transitionDuration: 9000,
