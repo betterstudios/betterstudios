c77b516773640d770ffcc9a904b095ef87b70754
munair <munair@qe.local>
2016-06-25 18:15:03 +0900

    load background image and add translucent white background to left hand side div

--- assets/scripts/vegas.js
+++ assets/scripts/vegas.js
@@ -9,13 +9,14 @@
 
 $(".front-page-header").vegas({
       timer: false,
+      delay: 17000,
       preload: true,
       overlay: 'https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/07.png',
       slides: [
-        { delay: 100, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "70%", transition: "zoomOut" },
-        { delay: 2600, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "50%", transition: "swirlRight" },
-        { delay: 2700, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "30%", transition: "zoomOut" },
-        { delay: 2700, src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", align: "10%", transition: "blur" },
+        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "70%", transition: "zoomOut" },
+        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "50%", transition: "swirlRight" },
+        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", valign: "30%", transition: "zoomOut" },
+        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", align: "10%", transition: "blur" },
       ],
       animation: 'kenburnsUpLeft',
       transitionDuration: 9000,
