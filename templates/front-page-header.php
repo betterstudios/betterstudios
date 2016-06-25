2ae6aee0293ffc24b24009ab211b0613d738419d
munair <munair@qe.local>
2016-06-25 23:26:44 +0900

    update mobile header

--- templates/front-page-header.php
+++ templates/front-page-header.php
@@ -1,25 +1,15 @@
 <header class="front-page-header">
 	<div class="header-content">
-		<div class="header-content-inner">
-			<div class="left-side">
-				<div class="underlay"> </div>
-				<h1>Creative Movement.<br> Serious Fitness.</h1>
+		<div class="left-side">
+			<div class="underlay"> </div>
+			<div class="header-content-inner">
+				<h1><?php bloginfo( 'name' ); ?></h1>
 				<hr>
-				<p>dance, play, sing and above all move creatively</p>
-				<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Find Out More</a>
-				<hr>
-				<h3 class="white-icons">
-					<div class="col-xs-4">
-						<a href="https://capoeirastudio.typeform.com/to/veVkqb"><i class="fa fa-child"></i></a> KIDS CLASS
-					</div>
-					<div class="col-xs-4">
-						<a href="mailto:learnprivately@capoeirastudio.com"><i class="fa fa-male"></i></a> LEARN PRIVATELY
-					</div>
-					<div class="col-xs-4">
-						<a href="mailto:freetrial@capoeirastudio.com"><i class="fa fa-check-circle-o"></i></a> FREE TRIAL
-					</div>
-				</h3>
+				<p><h1><?php bloginfo( 'description' ); ?></p>
+				<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">Find Out More</a>
 			</div>
+		</div>
+		<div class="right-side">
 		</div>
 	</div>
 </header>
