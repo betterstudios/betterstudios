4f2689e7f61f5a4a07a6ef934bfd6f650ba87602
munair <munair@qe.local>
2016-06-26 19:56:51 +0900

    implement new navigation, update style.css, and address layering in the header.

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,3 +1,22 @@
+<section id="navigation" class="section-navigation">
+    <div class="row">
+        <a class="logo" href="#" style="text-decoration: none; text-transform: uppercase;"><?php bloginfo('name'); ?></a>
+
+        <div class="mobile-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#introduction" style="text-decoration: none;"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>About</a></li>
+                <li><a href="#portfolios" style="text-decoration: none;"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Blog</a></li>
+                <li><a href="#footer" style="text-decoration: none;"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span>Contact</a></li>
+            </ul>
+        </nav>
+    </div> <!-- end row -->
+</section>
+
 <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
 	<div class="container-fluid">
 		<!-- Brand and toggle get grouped for better mobile display -->
