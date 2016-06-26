ec23bf2e12485b98626cd065733624170a54d059
munair <munair@qe.local>
2016-06-26 21:28:36 +0900

    update navscripts with jQuery function wrapper

--- templates/navscripts.php
+++ templates/navscripts.php
@@ -1,39 +1,42 @@
 <script type="text/javascript">
-// Sticky Header
-$(window).scroll(function() {
+(function($) {
+    "use strict"; // Start of use strict
 
-    if ($(window).scrollTop() > 100) {
-        $('.section-navigation').addClass('sticky');
-    } else {
-        $('.section-navigation').removeClass('sticky');
-    }
-});
-
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.section-navigation').hasClass('open-nav')) {
-        $('.section-navigation').removeClass('open-nav');
-    } else {
-        $('.section-navigation').addClass('open-nav');
-    }
-});
-
-$('.section-navigation li a').click(function() {
-    if ($('.section-navigation').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.section-navigation').removeClass('open-nav');
-    }
-});
-
-// navigation scroll lijepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-});
+	// Sticky Header in jQuery
+	$(window).scroll(function() {
+	
+	    if ($(window).scrollTop() > 100) {
+	        $('.section-navigation').addClass('sticky');
+	    } else {
+	        $('.section-navigation').removeClass('sticky');
+	    }
+	});
+	
+	// Mobile Navigation
+	$('.mobile-toggle').click(function() {
+	    if ($('.section-navigation').hasClass('open-nav')) {
+	        $('.section-navigation').removeClass('open-nav');
+	    } else {
+	        $('.section-navigation').addClass('open-nav');
+	    }
+	});
+	
+	$('.section-navigation li a').click(function() {
+	    if ($('.section-navigation').hasClass('open-nav')) {
+	        $('.navigation').removeClass('open-nav');
+	        $('.section-navigation').removeClass('open-nav');
+	    }
+	});
+	
+	$('nav a').click(function(event) {
+	    var id = $(this).attr("href");
+	    var offset = 70;
+	    var target = $(id).offset().top - offset;
+	    $('html, body').animate({
+	        scrollTop: target
+	    }, 500);
+	    event.preventDefault();
+	});
+	
+})(jQuery); // End of use strict
 </script>
-
