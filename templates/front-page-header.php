82c8393292bfcb25f0751712343924c0235fe770
munair <munair@qe.local>
2016-06-27 13:05:48 +0900

    shift button to the right hand side... make the right side the clickable side..

--- templates/front-page-header.php
+++ templates/front-page-header.php
@@ -6,7 +6,6 @@
 				<h1 class="wow fadeInDown" data-wow-delay="0.5s"><?php bloginfo( 'name' ); ?></h1>
 				<hr class="wow fadeInRight" data-wow-delay="1.0s">
 				<p class="wow fadeInUp" data-wow-delay="1.5s"><?php bloginfo( 'description' ); ?></p>
-				<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay="2s">Find Out More</a>
 			</div>
 		</div>
 		<div class="right-side">
@@ -17,16 +16,19 @@
 							<p class="ticker-headline wow flipInX" data-wow-delay="1.5s">
 								<a href="#"><strong>Hi</strong>... I am an avacado.</a>
 							</p>
+							<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay="2s">Purchase</a>
 						</div>
 						<div class="item">
 							<p class="ticker-headline wow flipInX" data-wow-delay="1.5s">
 								<a href="#"><strong>Hi</strong>... I am also an avacado.</a>
 							</p>
+							<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay="2s">Purchase</a>
 						</div>
 						<div class="item">
 							<p class="ticker-headline wow flipInX" data-wow-delay="1.5s">
 								<a href="#"><strong>Hi</strong>... I am an avacado too.</a>
 							</p>
+							<a href="#intro" class="btn btn-primary btn-xl page-scroll wow bounceIn" data-wow-delay="2s">Purchase</a>
 						</div>
 					</div>
 					<!-- Indicators -->
