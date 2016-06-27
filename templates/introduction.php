5e3bba18b681b86729a572944e841135c8990027
munair <munair@qe.local>
2016-06-27 18:46:06 +0900

    center introduction on viewport

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,4 +1,4 @@
-<section id="introduction" class="section-introduction bg-primary padded">
+<section id="introduction" class="section-introduction bg-primary viewport-centered">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 text-center">
@@ -6,7 +6,7 @@
 				<hr class="light">
 				<p class="text-faded">Better Studios started as a journal for those who invest in the body, mind, and studio. Recently, we added a store to the journal to make it easier for readers and contributors to purchase foods, natural oils, and other items that we discuss in this blog.</p>
 				<hr class="light">
-				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Learn More</a>
+				<a href="#about" class="btn btn-default btn-xl page-scroll wow flipInX" data-wow-delay=".1s">Learn More</a>
 			</div>
 		</div>
 	</div>
