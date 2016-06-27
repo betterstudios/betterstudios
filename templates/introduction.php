c4f1a8653de0a9e7ab8913a81c98ec8785327537
munair <munair@qe.local>
2016-06-27 18:29:27 +0900

    fix padding

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,11 +1,10 @@
-<section class="bg-primary" id="intro">
-	<div class="container">
+<section class="bg-primary" id="introduction padded">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-8 col-lg-offset-2 text-center">
-				<h2 class="section-heading">카포에라 스튜디오</h2>
+				<h2 class="section-heading">Body, Mind, and Studio</h2>
 				<hr class="light">
-				<p class="text-faded">Capoeira Studio의 강사진들과 수강생들은 카포에라라고 불리는 아프로 브라질리언 예술 행위의 창조적이고 아름다운 무브먼트를 통해 강인함, 유연성, 민첩성, 균형감각, 집중력, 리듬감 그리고 삶의 질을 향상시키는 것에 집중을 합니다.</p>
-				<p class="text-faded">The community of teachers and students at the Capoeira Studio are focused on improving strength, flexibility, agility, balance, concentration, rhythm, and quality of life through the creative and beautiful movement of the Afro-Brazilian artform called Capoeira.</p>
+				<p class="text-faded">Better Studios started as a journal for those who invest in the body, mind, and studio. Recently, we added a store to the journal to make it easier for readers and contributors to purchase foods, natural oils, and other items that we discuss in this blog.</p>
 				<hr class="light">
 				<a href="#about" class="btn btn-default btn-xl page-scroll wow bounceIn" data-wow-delay=".1s">더 보기 <br>Learn More</a>
 			</div>
