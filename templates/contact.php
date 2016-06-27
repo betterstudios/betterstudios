bf9dacb594137506e76384cda7dcfee33056b57a
munair <munair@qe.local>
2016-06-28 06:14:18 +0900

    update contact section

--- templates/contact.php
+++ templates/contact.php
@@ -1,26 +1,26 @@
-<section id="contact" class="section-contact viewport">
+<section id="contact" class="section-contact viewport bg-dark">
 	<div class="container viewport-centered">
 		<div class="row wow fadeIn" data-wow-delay="0.5s">
 			<div class="col-lg-8 col-lg-offset-2 text-center">
-				<h2 class="section-heading">Let's Get In Touch!</h2>
+				<h2 class="section-heading">Contact <strong>Me</strong></h2>
 				<hr class="primary">
-				<p>Need further information? Please contact us via telephone/KaKao, Facebook Messenger, YouTube, or email.</p>
+				<p>Need further information? Please contact me via telephone/KaKao, Facebook Messenger, YouTube, or email.</p>
 			</div>
 			<div class="col-lg-2 col-lg-offset-2 text-center">
-				<p><a href="tel:010-9090-9183"><i class="fa fa-phone fa-3x wow bounceIn" data-wow-delay=".1s"></i></a></p>
+				<p><a href="tel:010-9090-9183"><i class="fa fa-phone fa-3x wow flipInX" data-wow-delay="1.0s"></i></a></p>
 			</div>
 			<div class="col-lg-2 text-center">
-				<p><a href="http://www.facebook.com/capoeirastudio"><i class="fa fa-facebook fa-3x wow bounceIn" data-wow-delay=".1s"></i></a></p>
+				<p><a href="http://www.facebook.com/capoeirastudio"><i class="fa fa-facebook fa-3x wow flipInX" data-wow-delay="1.0s"></i></a></p>
 			</div>
 			<div class="col-lg-2 text-center">
-				<p><a href="http://www.youtube.com/capoeirastudio"><i class="fa fa-youtube fa-3x wow bounceIn" data-wow-delay=".1s"></i></a></p>
+				<p><a href="http://www.youtube.com/capoeirastudio"><i class="fa fa-youtube fa-3x wow flipInX" data-wow-delay="1.0s"></i></a></p>
 			</div>
 			<div class="col-lg-2 text-center">
-				<p><a href="mailto:info@capoeirastudio.com"><i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i></a></p>
+				<p><a href="mailto:info@capoeirastudio.com"><i class="fa fa-envelope-o fa-3x wow flipInX" data-wow-delay="1.0s"></i></a></p>
 			</div>
 			<div class="col-lg-8 col-lg-offset-2 text-center">
 				<hr class="primary">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6325.436276912393!2d126.978851!3d37.561704!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2f3864681b9%3A0xf3bd15bab5778767!2s93-3+Bukchang-dong%2C+Jung-gu%2C+Seoul%2C+South+Korea!5e0!3m2!1sen!2s!4v1439365275417" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6325.436276912393!2d126.978851!3d37.561704!3m2!1i1024!2i768!4f13.1!3m3!1m21.0s0x357ca2f3864681b9%3A0xf3bd15bab5778767!2s93-3+Bukchang-dong%2C+Jung-gu%2C+Seoul%2C+South+Korea!5e0!3m21.0sen!2s!4v1439365275417" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
