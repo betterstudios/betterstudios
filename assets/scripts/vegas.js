aae23ff155fcd32d7a129782f65a47ccc597cd95
munair <munair@qe.local>
2016-06-28 18:54:54 +0900

    redo slideshow timing... undo margin change to nav

--- assets/scripts/vegas.js
+++ assets/scripts/vegas.js
@@ -9,32 +9,30 @@
 
 $(".front-page-header").vegas({
       timer: false,
-      delay: 17000,
+      delay: 5000,
       preload: true,
       overlay: 'https://s3.amazonaws.com/betterstudios/08.png',
       slides: [
         { src: "https://s3.amazonaws.com/betterstudios/avacado-5000x3333px.png", valign: "70%", transition: "zoomOut" },
         { src: "https://s3.amazonaws.com/betterstudios/flaxseed-1280x918px.png", valign: "50%", transition: "swirlRight" },
-        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", align: "10%", transition: "blur" },
         { src: "https://s3.amazonaws.com/betterstudios/kukui-nut-2000x1250px.png", valign: "30%", transition: "zoomOut" },
       ],
       animation: 'kenburnsUpLeft',
-      transitionDuration: 9000,
+      transitionDuration: 2000,
 });
 
 $(".section-footer").vegas({
       timer: false,
-      delay: 17000,
+      delay: 5000,
       preload: true,
       overlay: 'https://s3.amazonaws.com/betterstudios/08.png',
       slides: [
         { src: "https://s3.amazonaws.com/betterstudios/avacado-5000x3333px.png", valign: "70%", transition: "zoomOut" },
         { src: "https://s3.amazonaws.com/betterstudios/flaxseed-1280x918px.png", valign: "50%", transition: "swirlRight" },
-        { src: "https://s3.amazonaws.com/betterstudios/eye-of-horus-header.jpg", align: "10%", transition: "blur" },
         { src: "https://s3.amazonaws.com/betterstudios/kukui-nut-2000x1250px.png", valign: "30%", transition: "zoomOut" },
       ],
       animation: 'kenburnsUpLeft',
-      transitionDuration: 9000,
+      transitionDuration: 2000,
 });
 
 
