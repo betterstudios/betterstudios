b6e8085de5521f92f2a2546e6880aa4d50c8a031
munair <munair@qe.local>
2016-06-29 06:16:50 +0900

    implement grey buttons and links

--- templates/about.php
+++ templates/about.php
@@ -4,7 +4,7 @@
 			<div class="col-lg-12 text-center">
 				<h2 class="section-heading">About <strong>Me</strong></h2>
 				<hr class="primary">
-				<img class="photo" src="https://s3.amazonaws.com/betterstudios/chris-dewberry-256x256px.png">
+				<img class="photo wow flipInX" data-wow-delay="1.5s" src="https://s3.amazonaws.com/betterstudios/chris-dewberry-256x256px.png">
 				<p class="text-muted">I am excited and humbled to share what I have learned about nutrition. I want to give back to the community some of the knowledge I have accumulated throughout the years with research and trial and error. I feel like I can bring some insight to people who are not only curious about different supplements, but what can truly help them in the long run in order to live a healthy lifestyle. </p>
 				<p class="text-muted">Nutrition is so very important in balancing out the body, so you can do things with a ‘well-oiled engine’. I really hope I can add some perspective, and I also hope that I can give back to you by sharing what I have gained. Please feel free to leave comments about anything. I will do my best to reply. I know that this site is a work in progress, but I want to take my time to thank you for coming to visit.</p>
 				<hr class="primary">
--- templates/footer.php
+++ templates/footer.php
@@ -4,13 +4,13 @@
 		<div class="second-layer">
 			<div class="row">
 		                <div class="col-md-3">
-		                        <h2 class="wow bounceIn" data-wow-delay=".1s">Site <strong>Map</strong></h2>
+		                        <h2 class="wow flipInX" data-wow-delay="1.0s">Site <strong>Map</strong></h2>
 		                        <p><a href="<?php echo get_site_url(); ?>">Home</a></p>
 		                        <p><a href="<?php echo get_site_url(); ?>/blog">Blog</a></p>
 		                        <p><a href="<?php echo get_site_url(); ?>/faq">FAQ</a></p>
 		                </div>
 		                <div class="col-md-3">
-		                        <h2 class="wow bounceIn" data-wow-delay=".1s">Recent <strong>Posts</strong></h2>
+		                        <h2 class="wow flipInX" data-wow-delay="1.0s">Recent <strong>Posts</strong></h2>
 					<?php
 						$args = array( 'numberposts' => '5', 'post_status' => 'publish' );
 						$recent_posts = wp_get_recent_posts( $args );
@@ -20,19 +20,20 @@
 					?>
 		                </div>
 		                <div class="col-md-3">
-		                        <h2 class="wow bounceIn" data-wow-delay=".1s">Site <strong>Admin</strong></h2>
+		                        <h2 class="wow flipInX" data-wow-delay="1.0s">Site <strong>Admin</strong></h2>
 		                        <p><a href="/wp-admin">Log In</a></p>
 		                        <p><a href="/logout">Log Out</a></p>
 		                </div>
 		                <div class="col-md-3">
-		                        <h2 class="wow bounceIn" data-wow-delay=".1s">My <strong>NEWSLETTER</strong></h2>
+		                        <h2 class="wow flipInX" data-wow-delay="1.0s">My <strong>NEWSLETTER</strong></h2>
 		                        <p><a href="http://eepurl.com/b3Y1Hz">Subscribe</a></p>
 		                        <p><a href="http://capoeirastudio.us13.list-manage.com/unsubscribe?u=3db435ce999565383d510d2cd&id=64f83024b3">Unsubscribe</a></p>
 		                </div>
 	                </div>
 			<div class="row">
 	                        <div class="col-md-4 col-md-offset-4 text-center">
-	                                <h5 id="copyright" class="notice">Copyright (c) 2016 <strong>Better Studios</strong>. All Rights <strong>Reserved</strong>.</h5>
+	                                <h5 id="copyright" class="notice"><strong>Copyright</strong> (c) 2016 <strong>Better Studios</strong>. All Rights Reserved.</h5>
+	                                <h5 id="maintenance" class="notice">Maintained by <strong>Quilombola Engineering</strong>.</h5>
 	                                <hr id="primary">
 	                                </a>
 	                        </div>
