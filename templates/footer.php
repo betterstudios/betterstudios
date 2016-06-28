a2e53e710441c3518b529d236acd322d9cdca70d
munair <munair@qe.local>
2016-06-28 23:30:09 +0900

    add second layer to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,38 +1,40 @@
 <!-- Footer -->
 <footer id="sitemap" class="section-footer viewport">
         <div class="container-fluid viewport-center">
-		<div class="row">
-	                <div class="col-md-4">
-	                        <h2 class="wow bounceIn" data-wow-delay=".1s">Site <strong>Map</strong></h2>
-	                        <p><a href="<?php echo get_site_url(); ?>">Home</a></p>
-	                        <p><a href="<?php echo get_site_url(); ?>/blog">Blog</a></p>
-	                        <p><a href="<?php echo get_site_url(); ?>/faq">FAQ</a></p>
+		<div class="second-layer">
+			<div class="row">
+		                <div class="col-md-4">
+		                        <h2 class="wow bounceIn" data-wow-delay=".1s">Site <strong>Map</strong></h2>
+		                        <p><a href="<?php echo get_site_url(); ?>">Home</a></p>
+		                        <p><a href="<?php echo get_site_url(); ?>/blog">Blog</a></p>
+		                        <p><a href="<?php echo get_site_url(); ?>/faq">FAQ</a></p>
+		                </div>
+		                <div class="col-md-4">
+		                        <h2 class="wow bounceIn" data-wow-delay=".1s">Recent <strong>Posts</strong></h2>
+					<?php
+						$args = array( 'numberposts' => '5', 'post_status' => 'publish' );
+						$recent_posts = wp_get_recent_posts( $args );
+						foreach( $recent_posts as $recent ){
+							echo '<p><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </p> ';
+						}
+					?>
+		                </div>
+		                <div class="col-md-4">
+		                        <h2 class="wow bounceIn" data-wow-delay=".1s">Site <strong>Admin</strong></h2>
+		                        <p><a href="/wp-admin">Log In</a></p>
+		                        <p><a href="/logout">Log Out</a></p>
+		                        <h2 class="wow bounceIn" data-wow-delay=".1s">My <strong>NEWSLETTER</strong></h2>
+		                        <p><a href="http://eepurl.com/b3Y1Hz">Subscribe</a></p>
+		                        <p><a href="http://capoeirastudio.us13.list-manage.com/unsubscribe?u=3db435ce999565383d510d2cd&id=64f83024b3">Unsubscribe</a></p>
+		                </div>
 	                </div>
-	                <div class="col-md-4">
-	                        <h2 class="wow bounceIn" data-wow-delay=".1s">Recent <strong>Posts</strong></h2>
-				<?php
-					$args = array( 'numberposts' => '5', 'post_status' => 'publish' );
-					$recent_posts = wp_get_recent_posts( $args );
-					foreach( $recent_posts as $recent ){
-						echo '<p><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </p> ';
-					}
-				?>
+			<div class="row">
+	                        <div class="col-md-4 col-md-offset-4 text-center">
+	                                <h5 id="featured-video-heading" class="section-heading">Copyright (c) 2016 Better Studios. All Rights Reserved.</h5>
+	                                <hr id="featured-hr">
+	                                </a>
+	                        </div>
 	                </div>
-	                <div class="col-md-4">
-	                        <h2 class="wow bounceIn" data-wow-delay=".1s">Site <strong>Admin</strong></h2>
-	                        <p><a href="/wp-admin">Log In</a></p>
-	                        <p><a href="/logout">Log Out</a></p>
-	                        <h2 class="wow bounceIn" data-wow-delay=".1s">My <strong>NEWSLETTER</strong></h2>
-	                        <p><a href="http://eepurl.com/b3Y1Hz">Subscribe</a></p>
-	                        <p><a href="http://capoeirastudio.us13.list-manage.com/unsubscribe?u=3db435ce999565383d510d2cd&id=64f83024b3">Unsubscribe</a></p>
-	                </div>
-                </div>
-		<div class="row">
-                        <div class="col-md-4 col-md-offset-4 text-center">
-                                <h5 id="featured-video-heading" class="section-heading">Copyright (c) 2016 Better Studios. All Rights Reserved.</h5>
-                                <hr id="featured-hr">
-                                </a>
-                        </div>
-                </div>
+        	</div>
         </div>
 </footer>
